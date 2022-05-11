107358176f26cfb11619b90d3c2caff63da87cd5
AlinazarBoynazarov <aboynaza@u.rochester.edu>
2022-05-10 22:38:08 -0400

    testing commit

--- frontend/src/OpportunitiesJSON.js
+++ frontend/src/OpportunitiesJSON.js
@@ -1,45 +1,58 @@
 const opportunities = [
-    {
-      id: 1,
-      name: 'United World Colleges',
-      href: 'https://www.tj.uwc.org/',
-      description: 'Group of international schools that aims to make education a force to unite peoples, nations, and cultures for peace and a sustainable future.',
-      notes: 'English required',
-      imageAlt: 'United World Colleges',
-      imageSrc: require('./images/UWCLogo.jpeg'),
-      category: 'school',
-    },
-    {
-      id: 2,
-      name: 'UCA Graduate Summer School',
-      href: 'https://ucentralasia.org/schools/school-of-professional-and-continuing-education/continuing-education-programme/spce-tajikistan',
-      description: 'Take summer classes at the presigious UCA Graduate school and learn from the best professors in the country.',
-      notes: 'High school seniors',
-      imageAlt: 'UCA Graduate Summer School',
-      imageSrc: require('./images/randomDude.png'),
-      category: 'school',
-    },
-    {
-      id: 3,
-      name: 'Media School CABAR Asia',
-      href: 'https://school.cabar.asia/en/',
-      description: 'Online courses for the youth.',
-      notes: 'Grade 12 only',
-      imageAlt: 'Media School CABAR Asia',
-      imageSrc: require('./images/CABAR.jpg'),
-      category: 'course',
-    },
-    {
-      id: 4,
-      name: 'Ilmhona Skills Accelerator',
-      href: 'https://ilmhona.org/',
-      description: 'Online courses for learning valuable tech skills to do with web development.',
-      notes: 'Russian required',
-      imageAlt: 'Ilmhona Skills Accelerator',
-      imageSrc: require('./images/ilmhona.jpeg'),
-      category: 'course',
-    },
-  ]
+  {
+    id: 1,
+    name: 'United World Colleges',
+    href: 'https://www.tj.uwc.org/',
+    description:
+      'Group of international schools that aims to make education a force to unite peoples, nations, and cultures for peace and a sustainable future.',
+    notes: 'English required',
+    imageAlt: 'United World Colleges',
+    imageSrc: require('./images/UWCLogo.jpeg'),
+    category: 'school',
+  },
+  {
+    id: 2,
+    name: 'UCA Graduate Summer School',
+    href: 'https://ucentralasia.org/schools/school-of-professional-and-continuing-education/continuing-education-programme/spce-tajikistan',
+    description:
+      'Take summer classes at the presigious UCA Graduate school and learn from the best professors in the country.',
+    notes: 'High school seniors',
+    imageAlt: 'UCA Graduate Summer School',
+    imageSrc: require('./images/randomDude.png'),
+    category: 'school',
+  },
+  {
+    id: 3,
+    name: 'Media School CABAR Asia',
+    href: 'https://school.cabar.asia/en/',
+    description: 'Online courses for the youth.',
+    notes: 'Grade 12 only',
+    imageAlt: 'Media School CABAR Asia',
+    imageSrc: require('./images/CABAR.jpg'),
+    category: 'course',
+  },
+  {
+    id: 4,
+    name: 'Ilmhona Skills Accelerator',
+    href: 'https://ilmhona.org/',
+    description:
+      'Online courses for learning valuable tech skills to do with web development.',
+    notes: 'Russian required',
+    imageAlt: 'Ilmhona Skills Accelerator',
+    imageSrc: require('./images/ilmhona.jpeg'),
+    category: 'course',
+  },
+  {
+    id: 4,
+    name: 'Tech Girls',
+    href: 'https://techgirlsglobal.org/',
+    description:
+      'International summer exchange program for young women to pursue careers in science and tech',
+    notes: 'An opportunity for girls in tech and science',
+    imageAlt: 'Ilmhona Skills Accelerator',
+    imageSrc: require('./images/ilmhona.jpeg'),
+    category: 'course',
+  },
+]
 
-  export default opportunities
-  
+export default opportunities
