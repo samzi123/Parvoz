44f71f6a7a0b8d01d0b44dcf3adb4e70b83022a7
Samuel Henderson <samzi.henderson@gmail.com>
2022-05-15 20:50:27 -0400

    adding stuff

--- frontend/src/screens/OpportunitiesScreen.js
+++ frontend/src/screens/OpportunitiesScreen.js
@@ -8,7 +8,7 @@ const OpportunitiesScreen = () => {
   return (
       <>
         <div className="bg-slate-900">
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="max-w-2xl mx-auto px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                 <h1 className="text-3xl font-extrabold tracking-tight text-gray-200 mb-8">Opportunities</h1>
 
                 <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
